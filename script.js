36e20a5ba7cbcc217df1112d2e4bc06ac85f0a3e
nagygergogit <nagygergomail@gmail.com>
2021-10-18 13:19:51 +0200

    final

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
 function genreView(genreList) {
-    let toReturn = "";
+  let toReturn = "";
   
-    for (const genreEach of genreList) {
-      toReturn += `
-        <li>${genreEach}</li>
-      `;
-    }
-  
-    return toReturn;
-  }
-  
-      function movieView(mov, index) {
-    return `
-      <div data-index="${index+1}" class="movie-card light">
-        <h2>${mov.title}</h2>
-        <div class="year">${mov.year}</div>
-        <div class="rate">${mov.rate}</div>
-        <ul class="genre">
-          ${genreView(mov.genre)}
-        </ul>
-      </div>
+  for (const genreEach of genreList) {
+    toReturn += `
+     <li>${genreEach}</li>
     `;
   }
-  function insertThenSelectByID(content, parent, where, id) 
   
-  {parent.insertAdjacentHTML(where, content);
-    return document.getElementById(id);
-          }
+  return toReturn;
+}
   
-  function changeTheme( {
-    const list = document.querySelectorAll(".movie-card");
+function movieView(mov, index) {
+  return `
+    <div data-index="${index+1}" class="movie-card light">
+      <h2>${mov.title}</h2>
+      <div class="year">${mov.year}</div>
+      <div class="rate">${mov.rate}</div>
+      <ul class="genre">
+        ${genreView(mov.genre)}
+      </ul>
+    </div>
+  `;
+}
+
+function insertThenSelectByID(content, parent, where, id) {
+  parent.insertAdjacentHTML(where, content);
+  return document.getElementById(id);
+}
   
-    for (const el of list) {
-      el.classList.toggle("light");
-      el.classList.toggle("dark");
-    }
+function changeTheme() {
+  const list = document.querySelectorAll(".movie-card");
+  
+  for (const el of list) {
+    el.classList.toggle("light");
+    el.classList.toggle("dark");
   }
+}
   
-  function loadEvent() {
-    const rootElement = document.getElementById("root");
+function loadEvent() {
+  const rootElement = document.getElementById("root");
   const buttonID = "change";
-          const buttonEl = insertThenSelectByID(`<button id="${buttonID}">Change theme</button>`, rootElement, "beforebegin", buttonID);
+  const buttonEl = insertThenSelectByID(`<button id="${buttonID}">Change theme</button>`, rootElement, "beforebegin", buttonID);
   
-    console.log(buttonEl);
+  console.log(buttonEl);
   
-    for (let i = 0; i < movies.length; i++) {
-      const movie = movies[i];
-      rootElement.insertAdjacentHTML("beforeend", movieView(movie, i));
-    }
-  
-    buttonEl.addEventListener("click", changeTheme);
-  
+  for (let i = 0; i < movies.length; i++) {
+    const movie = movies[i];
+    rootElement.insertAdjacentHTML("beforeend", movieView(movie, i));
   }
   
-  window.addEventListener("load", loadEvent);
+  buttonEl.addEventListener("click", changeTheme);
+  
+}
+  
+window.addEventListener("load", loadEvent);
